3899a4a83b1401251f69163b65fac273887b7b7b
Logan Arnett <logan@loganarnett.com>
2020-05-06 16:11:49 -0400

    adding styles

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,70 +1,110 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from "react";
+import Link from "gatsby-link";
+import { Helmet } from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
-interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
-  location: {
-    pathname: string
-  }
-  children: any
-}
-
-class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
-  public render() {
-    return (
-      <div>
-        <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {this.props.children()}
+  <div className="header-wrapper" id="home">
+    <section className="hero is-large">
+      <nav className="navbar is-transparent is-hidden-desktop">
+        <div className="navbar-brand">
+          <div className="navbar-burger burger" data-target="mobile-nav">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div id="mobile-nav" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <a className="navbar-item" href="#home">
+                Home
+              </a>
+            </div>
+            <div className="navbar-item">
+              <a className="navbar-item" href="#about-me">
+                About Me
+              </a>
+            </div>
+            <div className="navbar-item">
+              <a className="navbar-item" href="#services">
+                Services
+              </a>
+            </div>
+            <div className="navbar-item">
+              <a className="navbar-item" href="#skills">
+                Skills
+              </a>
+            </div>
+            <div className="navbar-item">
+              <a className="navbar-item" href="#my-work">
+                My Work
+              </a>
+            </div>
+            <div className="navbar-item">
+              <a className="navbar-item" href="#contact">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="hero-nav">
+        <div className="hero-nav--wrapper">
+          <div className="columns">
+            <div className="column is-12 hero-menu-desktop has-text-centered">
+              <ul>
+                <li className="is-active">
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                  <a href="#services">Services</a>
+                </li>
+                <li>
+                  <a href="#skills">Skills</a>
+                </li>
+                <li>
+                  <a href="#my-work">My Work</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    )
-  }
-}
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="subtitle">Hey there, I'm</h1>
+          <h2 className="title">Logan</h2>
+          <h1 className="subtitle profession">
+            A Full Stack Software <br /> and Cloud Engineer
+          </h1>
+        </div>
+      </div>
+    </section>
+  </div>
+);
 
-export default DefaultLayout
+const DefaultLayout: React.FC = (props) => {
+  return (
+    <>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: "description", content: "Sample" },
+          { name: "keywords", content: "sample, something" },
+        ]}
+      />
+      <Header />
+      <section>{props.children}</section>
+    </>
+  );
+};
+
+export default DefaultLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
+import * as React from "react";
+import Link from "gatsby-link";
+import "../styles/styles.scss";
+
+import DefaultLayout from "../layouts";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -7,30 +10,336 @@ interface IndexPageProps {
   data: {
     site: {
       siteMetadata: {
-        title: string
-      }
-    }
-  }
+        title: string;
+      };
+    };
+  };
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+const App: React.FC<IndexPageProps> = (props) => (
+  <DefaultLayout>
+    <div className="main-content">
+      <div className="section-color services" id="services">
+        <div className="container">
+          <div className="columns is-multiline">
+            <div
+              className="column is-12 about-me"
+              data-aos="fade-in"
+              data-aos-easing="linear"
+            >
+              <h1 className="title has-text-centered section-title">
+                Services
+              </h1>
+
+              <h2 className="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </h2>
+              <br />
+            </div>
+            <div className="columns is-12">
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fad fa-meteor fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fas fa-paint-brush fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fas fa-rocket fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+            </div>
+            <hr />
+            <div className="columns is-12">
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fas fa-upload fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fas fa-bus fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+              <div
+                className="column is-4 has-text-centered"
+                data-aos="fade-in"
+                data-aos-easing="linear"
+              >
+                <i className="fas fa-code fa-3x"></i>
+                <hr />
+                <h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
-}
+      <div className="section-dark my-work" id="my-work">
+        <div className="container">
+          <div
+            className="columns is-multiline"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div className="column is-12">
+              <h1 className="title has-text-centered section-title">My Work</h1>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=0')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=10')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=20')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=30')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=40')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=50')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=60')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+            <div className="column is-3">
+              <a href="#">
+                <figure
+                  className="image is-2by1 work-item"
+                  style={{
+                    backgroundImage: `url('https://picsum.photos/320/180?image=70')`,
+                  }}
+                ></figure>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section-light about-me" id="about-me">
+        <div className="container">
+          <div className="column is-12 about-me">
+            <h1 className="title has-text-centered section-title">About Me</h1>
+          </div>
+          <div className="columns is-multiline">
+            <div
+              className="column is-6 has-vertically-aligned-content"
+              data-aos="fade-right"
+            >
+              <p className="is-larger">
+                &emsp;&emsp;
+                <strong>
+                  Showcase is a modern, beautiful personal website template to
+                  showcase who you are, as well as projects you've worked on in
+                  the past.
+                </strong>
+              </p>
+              <br />
+              <p>
+                Showcase was built from the ground up with Bulma to be fast and
+                responsive out of the box with all source files well documented
+                for easy to customization. The Showcase template gives you a
+                personal space to share what you are all about as a creative
+                designer, developer, photographer, and more!
+              </p>
+              <br />
+              <div className="is-divider"></div>
+              <div className="columns about-links">
+                <div className="column">
+                  <p className="heading">
+                    <strong>Give me a ring</strong>
+                  </p>
+                  <p className="subheading">123-456-7890</p>
+                </div>
+                <div className="column">
+                  <p className="heading">
+                    <strong>Email Me</strong>
+                  </p>
+                  <p className="subheading">hello@example.com</p>
+                </div>
+                <div className="column">
+                  <p className="heading">
+                    <strong>View my portfolio</strong>
+                  </p>
+                  <p className="subheading">example.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="column is-6 right-image " data-aos="fade-left">
+              <img
+                className="is-rounded"
+                src="https://picsum.photos/id/366/600/375"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section-light contact" id="contact">
+        <div className="container">
+          <div
+            className="columns is-multiline"
+            data-aos="fade-in-up"
+            data-aos-easing="linear"
+          >
+            <div className="column is-12 about-me">
+              <h1 className="title has-text-centered section-title">
+                Get in touch
+              </h1>
+            </div>
+            <div className="column is-8 is-offset-2">
+              <form
+                action="https://formspree.io/email@example.com"
+                method="POST"
+              >
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control has-icons-left">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Ex. Jane Smith"
+                      name="Name"
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-user"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control has-icons-left">
+                    <input
+                      className="input"
+                      type="email"
+                      placeholder="Ex. hello@arctheme.com"
+                      name="Email"
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Message</label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      placeholder="Textarea"
+                      name="Message"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control ">
+                    <button className="button submit-button">
+                      Submit&nbsp;&nbsp;
+                      <i className="fas fa-paper-plane"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </DefaultLayout>
+);
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -40,4 +349,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
+
+export default App;
